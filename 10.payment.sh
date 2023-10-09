5247bfa1f568224d231a93234124b31859456fde
bindu <sindhudesalanka@gmail.com>
2023-10-09 19:17:55 +0530

    a

--- 10.payment.sh
+++ 10.payment.sh
@@ -7,8 +7,8 @@ yum install python36 gcc python3-devel -y &>>/tmp/roboshop.log
 echo -e "\e[32m adding user and location\e[0m"
 useradd roboshop &>>/tmp/roboshop.log
 mkdir /app &>>/tmp/roboshop.log
-rm -rf *
 cd /app
+rm -rf *
 echo -e "\e[32m downloading new content and dependencies to payment server\e[0m"
 curl -O https://roboshop-artifacts.s3.amazonaws.com/payment.zip &>>/tmp/roboshop.log
 unzip payment.zip &>>/tmp/payment.log
